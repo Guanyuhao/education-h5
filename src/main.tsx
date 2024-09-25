63140cc4e917e9656c9b90a587d68a7520c7582c
guanhaha <guanhaha@guanhahadeMacBook-Pro-2.local>
2024-09-25 13:21:49 +0800

    feat: chang loading

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Loading from './components/loading';
+// import Loading from './components/loading';
+import Loading from './components/loading/gifLoading';
+
 import Home from './pages/home';
 import { MapPage } from './router/index';
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Suspense fallback={<Loading size={120} />}>
+    {/* <Suspense fallback={<Loading size={120} />}> */}
+    <Suspense fallback={<Loading />}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />  {/* 首页 */}
